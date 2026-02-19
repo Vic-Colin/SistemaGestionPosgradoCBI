f60b339b1c28daecd088029b986fcbf4546ffdd4
CASH <CASH@DESKTOP-5220OB9>
2026-02-19 13:48:33 -0600

    Estructura de la Beca para su llenado y filtrado dinámico

--- src/main/java/uam/azc/sistemagestionposgradocbi/servlets/BecaServlet.java
+++ src/main/java/uam/azc/sistemagestionposgradocbi/servlets/BecaServlet.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package uam.azc.sistemagestionposgradocbi.servlets;
 
 import com.google.gson.Gson;
@@ -13,6 +10,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import uam.azc.sistemagestionposgradocbi.DAO.BecaDAO;
+import uam.azc.sistemagestionposgradocbi.modelo.Beca;
 
 /**
  *
